a0ad09732bc451c91c09ad89e48b6d9f073784f4
Luiz Otávio <luizotavioautomacao@gmail.com>
2022-10-22 01:51:28 -0300

    28 feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,13 +1,19 @@
+import { Authentication } from "../../../domain/usecases/authentication";
 import { InvalidParamError, MissingParamError } from "../../../presentation/errors";
 import { badRequest, serverError } from "../../../presentation/helpers/http-helper";
 import { Controller, EmailValidator, httpRequest, httpResponse } from "../signup/signup-protocols.index";
 
 export class LoginController implements Controller {
     private readonly emailValidator
-    constructor(emailValidator: EmailValidator) { this.emailValidator = emailValidator }
+    private readonly authentication
+
+    constructor(emailValidator: EmailValidator, authentication: Authentication) {
+        this.emailValidator = emailValidator
+        this.authentication = authentication
+    }
+
     async handle(htttRequest: httpRequest): Promise<httpResponse> {
         try {
-
             const { email, password } = htttRequest.body
             if (!email) {
                 return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -17,6 +23,7 @@ export class LoginController implements Controller {
             }
             const insValid = this.emailValidator.isValid(email)
             if (!insValid) return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+            await this.authentication.auth(email, password)
         } catch (error) {
             return serverError(error)
         }
