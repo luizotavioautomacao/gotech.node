198ef05bb46c9cf5ec33394cde53a2df66cb4bd2
Luiz Otávio <luizotavioautomacao@gmail.com>
2022-10-22 00:35:18 -0300

    27 refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,10 @@
 import { MissingParamError } from "../../../presentation/errors";
 import { badRequest } from "../../../presentation/helpers/http-helper";
-import { Controller, httpRequest, httpResponse } from "../signup/signup-protocols.index";
+import { Controller, EmailValidator, httpRequest, httpResponse } from "../signup/signup-protocols.index";
 
 export class LoginController implements Controller {
+    private readonly emailValidator
+    constructor(emailValidator: EmailValidator) { this.emailValidator = emailValidator }
     async handle(htttRequest: httpRequest): Promise<httpResponse> {
         if (!htttRequest.body.email) {
             return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
@@ -10,5 +12,6 @@ export class LoginController implements Controller {
         if (!htttRequest.body.password) {
             return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
         }
+        this.emailValidator.isValid(htttRequest.body.email)
     }
 }
