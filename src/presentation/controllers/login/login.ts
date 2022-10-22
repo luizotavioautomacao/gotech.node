1f605fcb7d56e4d04fd16ce2cb352fc59e88d563
Luiz Otávio <luizotavioautomacao@gmail.com>
2022-10-22 00:55:32 -0300

    27 refactor: change code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -6,13 +6,14 @@ export class LoginController implements Controller {
     private readonly emailValidator
     constructor(emailValidator: EmailValidator) { this.emailValidator = emailValidator }
     async handle(htttRequest: httpRequest): Promise<httpResponse> {
-        if (!htttRequest.body.email) {
+        const { email, password } = htttRequest.body
+        if (!email) {
             return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
         }
-        if (!htttRequest.body.password) {
+        if (!password) {
             return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
         }
-        const insValid = this.emailValidator.isValid(htttRequest.body.email)
+        const insValid = this.emailValidator.isValid(email)
         if (!insValid) return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
     }
 }
