78de0ff9018498d3485a10a8a0d569731803b944
Luiz Otávio <luizotavioautomacao@gmail.com>
2022-11-16 12:47:13 -0300

    feat: compare fields

---
+++ src/presentation/helpers/validators/compare-fileds.ts
@@ -0,0 +1,17 @@
+import { Validation } from "./validation";
+import { InvalidParamError } from "../../errors"
+
+export default class ComparteFieldsValidation implements Validation {
+    private readonly fieldName: string
+    private readonly fildToCompareName: string
+    constructor(fieldName: string, fildToCompareName: string) {
+        this.fieldName = fieldName
+        this.fildToCompareName = fildToCompareName
+    }
+
+    validate(input: any): Error {
+        if (input[this.fieldName]!=input[this.fildToCompareName]) {
+            return new InvalidParamError(this.fildToCompareName)
+        }
+    }
+}
